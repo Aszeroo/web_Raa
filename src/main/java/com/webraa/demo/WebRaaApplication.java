4969643ff99d3ae2663e095811d5dbe7b9183076
Aszeroo <aszeroop@gmail.com>
2022-12-06 14:58:44 +0700

    1

---
+++ src/main/java/com/webraa/demo/WebRaaApplication.java
@@ -0,0 +1,13 @@
+package com.webraa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRaaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/webraa/demo/controllers/AppController.java
@@ -0,0 +1,43 @@
+package com.webraa.demo.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AppController {
+
+    @GetMapping("login")
+    public String loginRoute() {
+        return "pages/login";
+    }
+
+    @GetMapping("register")
+    public String registerRoute() {
+        return "pages/register";
+    }
+
+    @GetMapping("MyAssessments")
+    public String MyAssessmentsRoute() {
+        return "pages/MyAssessments";
+    }
+
+    @GetMapping("assessment_configuration")
+    public String assessment_configurationRoute() {
+        return "pages/assessment_configuration";
+    }
+
+    @GetMapping("assessment")
+    public String assessmentRoute() {
+        return "pages/assessment";
+    }
+
+    @GetMapping("Results")
+    public String ResultsRoute() {
+        return "pages/Results";
+    }
+
+
+
+}
