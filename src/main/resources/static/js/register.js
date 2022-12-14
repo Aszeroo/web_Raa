26f682727111befbc54de85054ea191b2b067e1e
dekfewxz <dekfewxr@gmail.com>
2022-12-14 16:49:01 +0700

    login and register

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,4 +1,8 @@
-function inputdeta() {
+function inputdata() {
     var fname = document.getElementById("fname").value;
     document.getElementById("demo").innerHTML = fname;
 }
+const input = document.getElementById('field');
+input.setAttribute('required', '');
+input.removeAttribute('required');
+
