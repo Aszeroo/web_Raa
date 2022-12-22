a8f534202fa42804d608a8fe868cb9c642a6b398
Aszeroo <aszeroop@gmail.com>
2022-12-22 16:41:29 +0700

    update assessment

--- src/main/java/com/webraa/demo/controllers/AppController.java
+++ src/main/java/com/webraa/demo/controllers/AppController.java
@@ -57,7 +57,12 @@ public class AppController {
     }
 
     @GetMapping("assessment")
-    public String assessmentRoute() {
+    public String assessmentRoute(HttpSession session, HttpServletResponse response, HttpServletRequest request, Model model) {
+        model.addAttribute("username",session.getAttribute("username"));
+        model.addAttribute("firstname",session.getAttribute("firstname"));
+        model.addAttribute("lastname",session.getAttribute("lastname"));
+        model.addAttribute("companyName",session.getAttribute("companyName"));
+        model.addAttribute("type",session.getAttribute("type"));
         return "pages/assessment";
     }
 
