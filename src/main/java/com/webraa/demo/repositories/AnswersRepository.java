09ad38c6a95840293225d3660a9c50232495df7b
Prachaya Jantarapimporn <prachaya_j@d1asia.co.th>
2022-12-22 17:26:02 +0700

    api find summary answers

--- src/main/java/com/webraa/demo/repositories/AnswersRepository.java
+++ src/main/java/com/webraa/demo/repositories/AnswersRepository.java
@@ -2,6 +2,16 @@ package com.webraa.demo.repositories;
 
 import com.webraa.demo.entities.Answers;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AnswersRepository extends DataTablesRepository<Answers, String> {
+
+    @Query(value = "select topic_id,sum(ans_yes) ans_yes,sum(ans_no) ans_no from \n" +
+            "(select a.question_id,topic_id,answer,case when answer='yes' then 1 else 0 end as ans_yes,case when answer='no' then 1 else 0 end as ans_no from t_questions a \n" +
+            "join t_answers b on a.question_id=b.question_id where b.create_by = :userName) a group by topic_id order by topic_id", nativeQuery = true)
+    List<String> summaryAnswer(@Param("userName") String userName);
+
 }
